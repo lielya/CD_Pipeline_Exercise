da094f951bbf05518f0519c74527f878754caeaa
lielya <liel.nana1@gmail.com>
2023-01-08 00:11:45 +0200

    refactor: fix names and split functions

--- zip_job.py
+++ zip_job.py
@@ -1,31 +1,31 @@
 from zipfile import ZipFile
 
 VERSION = '1.2.0'
-TXT_SUFFIX = '.txt'
-ZIP_SUFFIX = '.zip'
-
 
 def main():
-    arr = ['a', 'b', 'c', 'd']
-    for file in arr:
-        txt_file = f'{file}{TXT_SUFFIX}'
+    files_names_to_create = ['a', 'b', 'c', 'd']
+    for file_name in files_names_to_create:
+        create_zip_with_text_file(file_name)
 
-        try:
-            create_txt_files(txt_file)
-            create_zip_files(file, txt_file)
+def create_zip_with_text_file(file_name: str):
+    txt_file_name = f'{file_name}.txt'
+    try:
+        create_txt_file(txt_file_name)
+        create_zip_file(txt_file_name)
+    
+    except OSError:
+        print(f"There is a problem creating the file: {file_name}")
 
-        except FileNotFoundError:
-            print("There is a problem creating this file")
+def create_txt_file(file_name: str):
+    with open(file_name, 'w') as text_file:
+        text_file.write("example")
 
 
-def create_txt_files(file):
-    new_file = open(file, 'w')
-
-
-def create_zip_files(file, txt_file):
-    zip_file_name = f'{file}_{VERSION}{ZIP_SUFFIX}'
+def create_zip_file(content_file_name: str):
+    content_file_prefix = '.'.join(content_file_name.split('.')[:-1])
+    zip_file_name = f'{content_file_prefix}_{VERSION}.zip'
     with ZipFile(zip_file_name, 'w') as zip_file:
-        zip_file.write(txt_file)
+        zip_file.write(content_file_name)
 
 
 if __name__ == '__main__':
